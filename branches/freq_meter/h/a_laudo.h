281a2a339953c32916cd7d23bc4e815ed38d70b4
rafael.g.gama <rafael.g.gama@dd0e6f3f-4bf6-1ab3-1e0a-dd9f5b42cf01>
2011-11-11 13:41:12 +0000

    Removing failed branch again...

--- branches/freq_meter/h/a_laudo.h
+++
@@ -1,5 +0,0 @@
-#include "defines.h"
-
-float get_baseline(float y[]);
-float get_amplitude(int size, float base, float y[], bool fbslope);
-float get_charge(int size, float base, float y[], bool fbslope);
